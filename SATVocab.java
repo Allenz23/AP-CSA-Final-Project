0370d6a6c21f5c4f4575dbaa9232665bb185c0df
Allenz23 <allenz23@nycstudents.net>
2025-06-03 00:04:19 +0000

    final draft

--- SATVocab.java
+++ SATVocab.java
@@ -1,8 +1,8 @@
 public class SATVocab extends Questions
 {
     public static ArrayList<Questions> vocabQuestions = new ArrayList<Questions>();
-    public SAT(String question, String explanation, String correctAnswer, String false1, String false2, String false3)
+    public SAT(String passage, String question, String explanation, String correctAnswer, String false1, String false2, String false3)
     {
-        super(question, explanation, correctAnswer, false1, false2, false3);
+        super(passage, question, explanation, correctAnswer, false1, false2, false3);
     }
 }
